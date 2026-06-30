2f787cf37759c5538b13bdf58853c7cc7bd951eb
Charmstok <3180292399@qq.com>
2026-06-30 18:17:38 +0800

    feat: Integrate llm scheduling into the scope of multi-objective optimization
    - Add data structures related to llm call

--- src/edge_tool_offloading/common/__init__.py
+++ src/edge_tool_offloading/common/__init__.py
@@ -1,6 +1,10 @@
 """Shared schemas and utilities."""
 
 from edge_tool_offloading.common.schemas import (
+    LLMCall,
+    LLMInstanceInfo,
+    LLMInstanceState,
+    LLMResult,
     ScheduleDecision,
     ToolCall,
     ToolResult,
@@ -10,6 +14,10 @@ from edge_tool_offloading.common.schemas import (
 )
 
 __all__ = [
+    "LLMCall",
+    "LLMInstanceInfo",
+    "LLMInstanceState",
+    "LLMResult",
     "ScheduleDecision",
     "ToolCall",
     "ToolResult",
